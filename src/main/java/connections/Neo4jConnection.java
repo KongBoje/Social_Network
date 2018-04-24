6bf01d7e6e945ecea1b4e04c06a4d84b12da6652
KongBoje <Michael_veilis@hotmail.com>
2018-04-24 17:48:44 +0200

    neo connect

---
+++ src/main/java/connections/Neo4jConnection.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connections;
+
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+
+/**
+ *
+ * @author Micha
+ */
+public class Neo4jConnection {
+    public static void main(String[] args) {
+        Driver driver = GraphDatabase.driver( 
+                "bolt://localhost:7687", 
+                AuthTokens.basic( "neo4jj", "class" ) );
+        Session session = driver.session();
+
+        // Run a query matching all nodes
+        StatementResult result = session.run( 
+                "MATCH (s:Person)" +
+                "RETURN s.name AS name, s.job AS job");
+
+        while ( result.hasNext() ) {
+            Record record = result.next();
+            System.out.println( record.get("name").asString() + " -> " + 
+                                record.get("job").asString());
+        }
+        session.close();
+        driver.close();
+    }
+}
